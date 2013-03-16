504c5e26c50e34393ec96c09bda3ed8af4e88d17
Peter WONG Wing Lun <luiges90@gmail.com>
2013-03-16 21:30:21 +0800

    修正絕對路徑

--- GameObjects/GameObjects/ExtensionInterface.cs
+++ GameObjects/GameObjects/ExtensionInterface.cs
@@ -39,6 +39,17 @@ public class ExtensionInterface
         }
     }
 
+    static string ProgramFilesx86()
+    {
+        if (8 == IntPtr.Size
+            || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
+        {
+            return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+        }
+
+        return Environment.GetEnvironmentVariable("ProgramFiles");
+    }
+
     public static void loadCompiledTypes()
     {
         if (compiledTypes == null)
@@ -53,7 +64,7 @@ public class ExtensionInterface
                 {
                     var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
                     var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll", 
-                        "C:/Program Files/Microsoft XNA/XNA Game Studio/v3.0/References/Windows/x86/Microsoft.Xna.Framework.dll", 
+                        ProgramFilesx86() + @"/Microsoft XNA/XNA Game Studio/v3.0/References/Windows/x86/Microsoft.Xna.Framework.dll", 
                         "GameObjects.dll", "GameGlobal.dll" });
                     parameters.GenerateExecutable = false;
                     CompilerResults results = csc.CompileAssemblyFromSource(parameters, file.Value);
