97ed41b7f5a61846a5b31ca8590ad61f66932dc3
Peter WONG Wing Lun <luiges90@gmail.com>
2013-03-30 18:11:19 +0800

    調整MakePublic.bat

--- WorldOfTheThreeKingdoms/bin/x86/Release/MakePublic.bat
+++ WorldOfTheThreeKingdoms/bin/x86/Release/MakePublic.bat
@@ -4,7 +4,6 @@ xcopy *.* ..\public /EXCLUDE:publicExclude.txt /e
 copy GameData\GameParametersPublic.xml ..\public\GameData\GameParameters.xml
 copy GameData\GlobalVariablesPublic.xml ..\public\GameData\GlobalVariables.xml
 copy GameData\Common\CommonDataPublic.mdb ..\public\GameData\Common\CommonData.mdb
-xcopy GameComponents\tupianwenzi\Data\meinvtupian\Public ..\public\GameComponents\tupianwenzi\Data\meinvtupian
 xcopy Resources\CreateChildrenTextFile\Public ..\public\Resources\CreateChildrenTextFile
 md ..\public\GameComponents\PersonPortrait\PlayerImage
 copy GameComponents\PersonPortrait\PlayerImage\9999.jpg ..\public\GameComponents\PersonPortrait\PlayerImage\9999.jpg
