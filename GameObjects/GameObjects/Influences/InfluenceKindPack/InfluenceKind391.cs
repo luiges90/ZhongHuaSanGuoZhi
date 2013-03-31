e7a1eda5fda20190e26226289d5704dd33fceb4d
Peter WONG Wing Lun <luiges90@gmail.com>
2013-03-31 17:52:13 +0800

    修正可能出現旳錯誤

--- GameObjects/GameObjects/Influences/InfluenceKindPack/InfluenceKind391.cs
+++ GameObjects/GameObjects/Influences/InfluenceKindPack/InfluenceKind391.cs
@@ -32,10 +32,13 @@
                 int num3 = source.GetStratagemSuccessChanceCredit(troop, source.InevitableRaoluanOnLowerIntelligence || source.InevitableStratagemOnLowerIntelligence, (troop.NeverBeIntoChaos || troop.OutburstNeverBeIntoChaos) || troop.InvincibleRaoluan, troop.InvincibleStratagemFromLowerIntelligence);
                 if (num3 > 0)
                 {
-                    num3 = ((num3 + ((troop.Army.Scales - 5) * 5)) * troop.FightingForce) / pureFightingForce;
-                    if (troop.Status == TroopStatus.混乱)
+                    if (pureFightingForce == 0)
                     {
-                        num3 /= GameObject.Square(source.ViewingHostileTroopCount) + 2;
+                        num3 = 1000000;
+                    }
+                    else
+                    {
+                        num3 = (((num3 + ((100 - troop.Morale) / 2)) + ((troop.Army.Scales - 5) * 5)) * troop.PureFightingForce) / pureFightingForce;
                     }
                     num += num3;
                 }
