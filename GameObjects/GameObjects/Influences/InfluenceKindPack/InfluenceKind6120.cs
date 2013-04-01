e37f6143ffd7b2463206e634a9a8be595a47b938
Peter WONG Wing Lun <luiges90@gmail.com>
2013-04-01 23:10:46 +0800

    修正部分影響出錯

--- GameObjects/GameObjects/Influences/InfluenceKindPack/InfluenceKind6120.cs
+++ GameObjects/GameObjects/Influences/InfluenceKindPack/InfluenceKind6120.cs
@@ -8,25 +8,9 @@
     {
         private int increment;
 
-        public override void ApplyInfluenceKind(Person person)
-        {
-            if (person.LocationArchitecture != null)
-            {
-                person.LocationArchitecture.noEscapeChance += this.increment;
-            }
-        }
-
         public override void ApplyInfluenceKind(Architecture a)
         {
             a.noEscapeChance += this.increment;
-        }
-
-        public override void PurifyInfluenceKind(Person person)
-        {
-            if (person.LocationArchitecture != null)
-            {
-                person.LocationArchitecture.noEscapeChance -= this.increment;
-            }
         }
 
         public override void PurifyInfluenceKind(Architecture a)
