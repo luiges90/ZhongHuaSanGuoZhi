7331818380cf71c6da6aa171b0a83d977530b1bd
YPZhou <argoran@gmail.com>
2013-04-18 15:22:42 +0300

    Update MainGame.cs

--- WorldOfTheThreeKingdoms/YouxiJincheng/MainGame.cs
+++ WorldOfTheThreeKingdoms/YouxiJincheng/MainGame.cs
@@ -143,6 +143,11 @@ namespace WorldOfTheThreeKingdoms
                 }
             }
         }
+        
+        public void SaveGameWhenCrash(String _savePath)
+        {
+            mainGameScreen.SaveGameWhenCrash(_savePath);
+        }
 
         public List<int> InitializationFactionIDs
         {
