ef1c6733c782432acd5ef7bd8353d2f1e3ea21c2
Peter WONG Wing Lun <luiges90@gmail.com>
2013-05-19 14:35:29 +0800

    減少忠誠下降

--- GameObjects/GameObjects/Person.cs
+++ GameObjects/GameObjects/Person.cs
@@ -2778,7 +2778,7 @@
             if (((this.BelongedFaction != null) && (((this.LocationArchitecture == null) || this.IsCaptive) || !this.LocationArchitecture.DayLocationLoyaltyNoChange))
                 && (((this.LocationTroop == null) || this.IsCaptive) || !this.LocationTroop.DayLocationLoyaltyNoChange)
                 && GameObject.Chance(100 - this.personalLoyalty * 25)  
-                && (this.Loyalty <= 110) )
+                && (this.Loyalty <= 110) && GameObject.Chance(50))
             {
                 int idealOffset = GetIdealOffset(this, this.BelongedFaction.Leader);
                 //亲爱武将性格差调整
