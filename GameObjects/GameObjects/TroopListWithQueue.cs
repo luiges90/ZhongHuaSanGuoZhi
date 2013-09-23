d9333410a84e9223d603fd60e6e3ca3f98a565a3
Peter WONG Wing Lun <luiges90@gmail.com>
2013-09-23 23:40:26 +0800

    AI部隊先移動後攻擊，修正AI攻擊不移動

--- GameObjects/GameObjects/TroopListWithQueue.cs
+++ GameObjects/GameObjects/TroopListWithQueue.cs
@@ -115,23 +115,6 @@
                             item.OperationDone = true;
                             break;
                         }
-                        if (!(item.HasToDoCombatAction || !item.ToDoCombatAction()))
-                        {
-                            item.HasToDoCombatAction = true;
-                            this.CurrentQueue.Enqueue(item);
-                            break;
-                        }
-                        if (item.HasToDoCombatAction)
-                        {
-                            item.HasToDoCombatAction = false;
-                            item.DoCombatAction();
-                            this.CurrentQueue.Enqueue(item);
-                            break;
-                        }
-                        if (this.troopQueue.Count > 0)
-                        {
-                            this.CurrentQueue.Enqueue(this.troopQueue.Dequeue());
-                        }
                         if (item.Destroyed || (item.Status != TroopStatus.一般))
                         {
                             item.MovabilityLeft = -1;
@@ -142,6 +125,24 @@
                             this.TroopChangeRealDestination(item);
                             this.TroopMoveThread(item);
                         }
+
+                        if (item.MovabilityLeft <= 0)
+                        {
+                            if (!item.HasToDoCombatAction &&item.ToDoCombatAction())
+                            {
+                                item.HasToDoCombatAction = true;
+                                this.CurrentQueue.Enqueue(item);
+                                break;
+                            }
+                            if (item.HasToDoCombatAction)
+                            {
+                                item.HasToDoCombatAction = false;
+                                item.DoCombatAction();
+                                this.CurrentQueue.Enqueue(item);
+                                break;
+                            }
+                        }
+
                         if ((!item.OperationDone && item.OffenceOnlyBeforeMove) && (item.Position != item.PreviousPosition))
                         {
                             item.OperationDone = true;
@@ -151,6 +152,12 @@
                             this.CurrentTroop = item;
                             break;
                         }
+
+                        if (this.troopQueue.Count > 0)
+                        {
+                            this.CurrentQueue.Enqueue(this.troopQueue.Dequeue());
+                        }
+
                         if (!this.queueEnded)
                         {
                             if (item.MovabilityLeft > 0)
