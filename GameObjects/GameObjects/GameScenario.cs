5d2ae8e7659563685d225aff104905dd8dcfa89a
Peter WONG Wing Lun <luiges90@gmail.com>
2013-10-15 19:33:32 +0800

    修正錯誤

--- GameObjects/GameObjects/GameScenario.cs
+++ GameObjects/GameObjects/GameScenario.cs
@@ -2532,8 +2532,8 @@
                 hatedIds.Add(person.ID, intArray);
                
                 person.Skills.LoadFromString(this.GameCommonData.AllSkills, reader["Skills"].ToString());
-                person.RealPersonalTitle = null;
-                person.RealCombatTitle = null;
+                person.RealPersonalTitle = this.GameCommonData.AllTitles.GetTitle((short)reader["PersonalTitle"]);
+                person.RealCombatTitle = this.GameCommonData.AllTitles.GetTitle((short)reader["CombatTitle"]);
                 person.StudyingTitle = this.GameCommonData.AllTitles.GetTitle((short)reader["StudyingTitle"]);
                 person.huaiyun = (bool)reader["huaiyun"];
                 person.faxianhuaiyun = (bool)reader["faxianhuaiyun"];
