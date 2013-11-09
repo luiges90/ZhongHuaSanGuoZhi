81477db0e05631db091310efeff23f333e5229db
Peter WONG Wing Lun <luiges90@gmail.com>
2013-11-09 13:17:19 +0800

    獲得稱號事件先移除舊有同種類稱號

--- GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect310.cs
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect310.cs
@@ -1,7 +1,9 @@
 ﻿namespace GameObjects.ArchitectureDetail.EventEffect
 {
     using GameObjects;
+    using GameObjects.PersonDetail;
     using System;
+    using System.Collections.Generic;
 
     internal class EventEffect310 : EventEffectKind
     {
@@ -9,7 +11,17 @@
 
         public override void ApplyEffectKind(Person person, Event e)
         {
-            GameObjects.PersonDetail.Title title = person.Scenario.GameCommonData.AllTitles.GetTitle(increment);
+            Title title = person.Scenario.GameCommonData.AllTitles.GetTitle(increment);
+
+            List<Title> oldTitles = new List<Title>(person.RealTitles);
+            foreach (Title t in oldTitles)
+            {
+                if (t.Kind == title.Kind)
+                {
+                    t.Influences.PurifyInfluence(person, GameObjects.Influences.Applier.Title, t.ID);
+                    person.RealTitles.Remove(t);
+                }
+            }
             person.RealTitles.Add(title);
         }
 
