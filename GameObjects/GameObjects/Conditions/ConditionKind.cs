4aefa01d1f3c94933e81e5e73bf1601874198abe
Peter WONG <luiges90@gmail.com>
2014-10-11 13:26:33 +0800

    建築、勢力條件用於建築、武將、部隊

--- GameObjects/GameObjects/Conditions/ConditionKind.cs
+++ GameObjects/GameObjects/Conditions/ConditionKind.cs
@@ -7,6 +7,10 @@
     {
         public virtual bool CheckConditionKind(Architecture architecture, Event e)
         {
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return architecture.BelongedFaction != null && this.CheckConditionKind(architecture.BelongedFaction, e);
+            }
             return false;
         }
 
@@ -17,16 +21,32 @@
 
         public virtual bool CheckConditionKind(Person person, Event e)
         {
+            if (this.ID >= 2000 && this.ID < 3000)
+            {
+                return person.LocationArchitecture != null && this.CheckConditionKind(person.LocationArchitecture, e);
+            }
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return person.BelongedFaction != null && this.CheckConditionKind(person.BelongedFaction, e);
+            }
             return false;
         }
 
         public virtual bool CheckConditionKind(Troop troop, Event e)
         {
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return troop.BelongedFaction != null && this.CheckConditionKind(troop.BelongedFaction, e);
+            }
             return false;
         }
 
         public virtual bool CheckConditionKind(Architecture architecture)
         {
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return architecture.BelongedFaction != null && this.CheckConditionKind(architecture.BelongedFaction);
+            }
             return false;
         }
 
@@ -37,11 +57,23 @@
 
         public virtual bool CheckConditionKind(Person person)
         {
+            if (this.ID >= 2000 && this.ID < 3000)
+            {
+                return person.LocationArchitecture != null && this.CheckConditionKind(person.LocationArchitecture);
+            }
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return person.BelongedFaction != null && this.CheckConditionKind(person.BelongedFaction);
+            }
             return false;
         }
 
         public virtual bool CheckConditionKind(Troop troop)
         {
+            if (this.ID >= 3000 && this.ID < 4000)
+            {
+                return troop.BelongedFaction != null && this.CheckConditionKind(troop.BelongedFaction);
+            }
             return false;
         }
 
