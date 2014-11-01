17d350d31feb4574ba15a53bbf8fa72b025adf1e
Peter WONG <luiges90@gmail.com>
2014-11-01 20:42:18 +0800

    調整聲討AI

--- GameObjects/GameObjects/Faction.cs
+++ GameObjects/GameObjects/Faction.cs
@@ -3552,7 +3552,7 @@
                     fc++;
                     if (((base.Scenario.DiplomaticRelations.GetDiplomaticRelation(base.Scenario, target.ID, f.ID).Relation +
                         Person.GetIdealOffset(target.Leader, f.Leader) * 1.5) < 0
-                        && (GameObject.Chance(60) || simulate) && !f.IsFriendly(target) && (f.adjacentTo(target) || GameObject.Chance(30)))
+                        && (GameObject.Chance(60) || simulate) && !f.IsFriendly(target) && (f.adjacentTo(target) || GameObject.Chance(30) || simulate))
                         )
                     {
                         encircleList.Add(f);
