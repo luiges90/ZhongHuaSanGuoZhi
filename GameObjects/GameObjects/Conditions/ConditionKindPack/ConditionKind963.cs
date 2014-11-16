23e7416ebd63d16365c407fc8260b941e0ac6321
kpxp <gk19839601@qq.com>
2014-11-16 22:04:22 +0800

    新增条件961-963用于称号

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind963.cs
@@ -0,0 +1,27 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind963 : ConditionKind
+    {
+        private int number = 0;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.Father != null && person.Father.RealTitles.Contains(person.Scenario.GameCommonData.AllTitles.GetTitle(number));
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.number = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
