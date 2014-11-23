607d2b9b2200cd742c5028691fba1d4b57f5aba9
Peter WONG <luiges90@gmail.com>
2014-11-23 19:13:14 +0800

    merge

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind961.cs
@@ -0,0 +1,15 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind961 : ConditionKind
+    {
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.BelongedFaction != null && (person.Spouse.Father == person.BelongedFaction.Leader || person.Spouse.Mother == person.BelongedFaction.Leader);
+        }
+    }
+}
+
