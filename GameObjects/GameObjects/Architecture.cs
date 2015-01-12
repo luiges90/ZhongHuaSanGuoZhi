1824e02c751f0e54147bff21802bd5d9a9f21083
Peter WONG <luiges90@gmail.com>
2015-01-12 21:25:23 +0800

    修正錯誤

--- GameObjects/GameObjects/Architecture.cs
+++ GameObjects/GameObjects/Architecture.cs
@@ -2394,8 +2394,11 @@
                         upgradable.Add(mk);
                     }
                 }
-                
-                this.LevelUpMilitary(military, upgradable[GameObject.Random(upgradable.Count)]);
+
+                if (upgradable.Count > 0)
+                {
+                    this.LevelUpMilitary(military, upgradable[GameObject.Random(upgradable.Count)]);
+                }
             }
         }
 
