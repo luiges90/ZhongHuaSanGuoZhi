e99cc5eacd945c8dfbebbc40753540ac68c4d181
kpxp <gk19839601@qq.com>
2015-01-23 17:57:32 +0800

    删除势力最大兵数

--- GameObjects/GameObjects/Architecture.cs
+++ GameObjects/GameObjects/Architecture.cs
@@ -8624,10 +8624,6 @@
                 {
                     return false;
                 }
-                if (this.BelongedFaction.Army > (int)(this.BelongedFaction.Population * GlobalVariables.ArmyPopulationCap)) //势力兵力超过上限时，不能新编
-                {
-                    return false;
-                }
                 foreach (MilitaryKind kind in this.BelongedFaction.AvailableMilitaryKinds.MilitaryKinds.Values)
                 {
                     if (kind.CreateAvail(this))
@@ -9518,10 +9514,6 @@
                     return false;
                 }
                 if (GlobalVariables.PopulationRecruitmentLimit && (this.ArmyQuantity > this.Population))
-                {
-                    return false;
-                }
-                if (this.BelongedFaction != null && this.BelongedFaction.Army > (int)(this.BelongedFaction.Population * GlobalVariables.ArmyPopulationCap)) //势力兵力超过上限时，不能补充
                 {
                     return false;
                 }
