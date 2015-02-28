5ce0b8d1c2362faa6f8e5d39ed2b0973f480f86c
Peter WONG <luiges90@gmail.com>
2015-02-28 16:04:54 +0800

    更新劇本生成器

--- ScenarioGenerator/Form1.cs
+++ ScenarioGenerator/Form1.cs
@@ -6,6 +6,7 @@ using System.Data.OleDb;
 using System.IO;
 using GameGlobal;
 using GameObjects;
+using GameObjects.PersonDetail;
 
 namespace ScenarioGenerator
 {
@@ -40,9 +41,49 @@ namespace ScenarioGenerator
                 this.openedFileName = this.openScenFileDialog.FileName;
                 this.scen.LoadGameScenarioFromDatabase(this.connectionStringBuilder.ConnectionString);
 
-                lblFilename.Text = this.scen.ScenarioTitle;
+                updateUIByScenario();
+
                 btnGenerate.Enabled = true;
             }
+        }
+
+        private void updateUIByScenario()
+        {
+            lblFilename.Text = this.scen.ScenarioTitle;
+
+            PersonGeneratorSetting setting = ((PersonGeneratorSetting)this.scen.GameCommonData.PersonGeneratorSetting);
+
+            tbFemaleChance.Text = setting.femaleChance.ToString();
+            tbBornYearLo.Text = setting.bornLo.ToString();
+            tbBornYearHi.Text = setting.bornHi.ToString();
+            tbDebutAgeLo.Text = setting.debutLo.ToString();
+            tbDebutAgeHi.Text = setting.debutHi.ToString();
+            tbAgeLo.Text = setting.dieLo.ToString();
+            tbAgeHi.Text = setting.dieHi.ToString();
+
+            List<GameObject> types = this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects;
+
+            lblPersonType0.Text = ((PersonGeneratorType)types[0]).Name;
+            lblPersonType1.Text = ((PersonGeneratorType)types[1]).Name;
+            lblPersonType2.Text = ((PersonGeneratorType)types[2]).Name;
+            lblPersonType3.Text = ((PersonGeneratorType)types[3]).Name;
+            lblPersonType4.Text = ((PersonGeneratorType)types[4]).Name;
+            lblPersonType5.Text = ((PersonGeneratorType)types[5]).Name;
+            lblPersonType6.Text = ((PersonGeneratorType)types[6]).Name;
+            lblPersonType7.Text = ((PersonGeneratorType)types[7]).Name;
+            lblPersonType8.Text = ((PersonGeneratorType)types[8]).Name;
+            lblPersonType9.Text = ((PersonGeneratorType)types[9]).Name;
+
+            tbPersonType0.Text = ((PersonGeneratorType)types[0]).generationChance.ToString();
+            tbPersonType1.Text = ((PersonGeneratorType)types[1]).generationChance.ToString();
+            tbPersonType2.Text = ((PersonGeneratorType)types[2]).generationChance.ToString();
+            tbPersonType3.Text = ((PersonGeneratorType)types[3]).generationChance.ToString();
+            tbPersonType4.Text = ((PersonGeneratorType)types[4]).generationChance.ToString();
+            tbPersonType5.Text = ((PersonGeneratorType)types[5]).generationChance.ToString();
+            tbPersonType6.Text = ((PersonGeneratorType)types[6]).generationChance.ToString();
+            tbPersonType7.Text = ((PersonGeneratorType)types[7]).generationChance.ToString();
+            tbPersonType8.Text = ((PersonGeneratorType)types[8]).generationChance.ToString();
+            tbPersonType9.Text = ((PersonGeneratorType)types[9]).generationChance.ToString();
         }
 
         private void btnGenerate_Click(object sender, EventArgs e)
@@ -61,7 +102,7 @@ namespace ScenarioGenerator
             File.Copy(Application.StartupPath + "/GameData/Common/SaveTemplate.mdb", generatedFileName);
 
             this.connectionStringBuilder.DataSource = generatedFileName;
-            this.scen.SaveGameScenarioToDatabase(this.connectionStringBuilder.ConnectionString, true, this.scen.UsingOwnCommonData);
+            this.scen.SaveGameScenarioToDatabase(this.connectionStringBuilder.ConnectionString, true, true);
 
             MessageBox.Show("劇本已生成，檔案為" + generatedFileName);
 
@@ -72,22 +113,82 @@ namespace ScenarioGenerator
         {
             try
             {
-                int count = GameObject.Random(int.Parse(tbAddPersonLo.Text), int.Parse(tbAddPersonHi.Text));
-
-                int joinChance = int.Parse(tbJoinedFactionChance.Text);
-                for (int i = 0; i < count; ++i)
-                {
-                    Architecture location = (Architecture) scen.Architectures[GameObject.Random(scen.Architectures.Count)];
-                    Person p = Person.createPerson(scen, location, null, false);
-                    if (GameObject.Chance(joinChance) && location.BelongedFaction != null)
-                    {
-                        p.ChangeFaction(location.BelongedFaction);
-                    }
-                }
+                generatePerson();
             }
             catch (FormatException ex)
             {
                 MessageBox.Show("無效輸入：" + ex.Message);
+            }
+        }
+
+        private void deletePerson()
+        {
+            int toDeleteCnt = GameObject.Random(int.Parse(tbDeletePersonLo.Text), int.Parse(tbDeletePersonHi.Text));
+            int toDeleteAby = GameObject.Random(int.Parse(tbDeletePersonAbyLo.Text), int.Parse(tbDeletePersonAbyHi.Text));
+            int toDeleteTotalAby = GameObject.Random(int.Parse(tbDeletePersonTAbyLo.Text), int.Parse(tbDeletePersonTAbyHi.Text));
+            bool allowDeleteKing = cbDeleteLeader.Checked;
+
+        }
+
+        private void generatePerson()
+        {
+            this.scen.GameCommonData.PersonGeneratorSetting.bornLo = int.Parse(tbBornYearLo.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.bornHi = int.Parse(tbBornYearHi.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.debutLo = int.Parse(tbDebutAgeLo.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.debutHi = int.Parse(tbDebutAgeHi.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.dieLo = int.Parse(tbAgeLo.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.dieHi = int.Parse(tbAgeHi.Text);
+            this.scen.GameCommonData.PersonGeneratorSetting.debutAtLeast = int.Parse(tbDebutAtLeast.Text);
+
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[0]).generationChance = int.Parse(tbPersonType0.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[1]).generationChance = int.Parse(tbPersonType1.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[2]).generationChance = int.Parse(tbPersonType2.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[3]).generationChance = int.Parse(tbPersonType3.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[4]).generationChance = int.Parse(tbPersonType4.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[5]).generationChance = int.Parse(tbPersonType5.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[6]).generationChance = int.Parse(tbPersonType6.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[7]).generationChance = int.Parse(tbPersonType7.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[8]).generationChance = int.Parse(tbPersonType8.Text);
+            ((PersonGeneratorType)this.scen.GameCommonData.AllPersonGeneratorTypes.GameObjects[9]).generationChance = int.Parse(tbPersonType9.Text);
+
+            int count = GameObject.Random(int.Parse(tbAddPersonLo.Text), int.Parse(tbAddPersonHi.Text));
+
+            int joinChance = int.Parse(tbJoinedFactionChance.Text);
+            for (int i = 0; i < count; ++i)
+            {
+                bool joined = GameObject.Chance(joinChance);
+
+                Architecture location = null;
+                if (joined)
+                {
+                    ArchitectureList candidates = new ArchitectureList();
+                    foreach (Architecture j in scen.Architectures)
+                    {
+                        if (j.BelongedFaction != null)
+                        {
+                            candidates.Add(j);
+                        }
+                    }
+                    if (candidates.Count > 0)
+                    {
+                        location = (Architecture)candidates.GetRandomObject();
+                    }
+                    else
+                    {
+                        joined = false;
+                    }
+                }
+
+                if (!joined)
+                {
+                    location = (Architecture)scen.Architectures.GetRandomObject();
+                }
+
+                Person p = Person.createPerson(scen, location, null, false);
+                if (joined && location.BelongedFaction != null)
+                {
+                    p.ChangeFaction(location.BelongedFaction);
+                }
             }
         }
 
