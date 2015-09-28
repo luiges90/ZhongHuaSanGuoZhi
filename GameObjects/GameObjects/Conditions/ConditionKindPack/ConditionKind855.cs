dbcc3b5e2bc6f51e6e3c34fbd1db1337cf5755c0
Peter WONG <luiges90@gmail.com>
2015-09-28 18:41:13 +0800

    855 - 與勢力君主親密度少於

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind855.cs
@@ -0,0 +1,29 @@
+namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+    using System.Collections.Generic;
+
+    internal class ConditionKind855 : ConditionKind
+    {
+        private int number = 0;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.BelongedFaction != null && person.GetRelation(person.BelongedFaction.Leader) < this.number;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.number = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+
