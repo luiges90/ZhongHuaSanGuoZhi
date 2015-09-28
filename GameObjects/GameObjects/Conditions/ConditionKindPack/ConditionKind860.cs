f92c9d27fb55a661d9f77c73958bbff96054e7ac
Peter WONG <luiges90@gmail.com>
2015-09-28 18:41:44 +0800

    860 勢力君主對自己的親密度達到

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind860.cs
@@ -0,0 +1,29 @@
+namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+    using System.Collections.Generic;
+
+    internal class ConditionKind860 : ConditionKind
+    {
+        private int number = 0;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.BelongedFaction != null && person.BelongedFaction.Leader.GetRelation(person) >= this.number;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.number = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+
