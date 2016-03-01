d2f3ad07ab2906935008bbe44402c19c946e2890
Peter WONG <luiges90@gmail.com>
2016-03-01 20:05:22 +0800

    incorporate kpxp commits

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind965.cs
@@ -0,0 +1,14 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind965 : ConditionKind
+    {
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.Sex && !person.huaiyun;
+        }
+    }
+}
