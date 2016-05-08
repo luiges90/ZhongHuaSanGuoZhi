28bf93aec6e7cb36a2e3878bb00c1e41affd428f
Peter Wong <luiges90@gmail.com>
2016-05-08 22:49:52 +0800

    調整調動AI

--- GameObjects/GameObjects/Faction.cs
+++ GameObjects/GameObjects/Faction.cs
@@ -1326,14 +1326,19 @@
                         minTroop.Add(a, a.TroopReserveScale); // defensiveCampaign will deal with this
                         urgent = true;
                     }
-                    else if (a.FrontLine || a.IsNetLosingPopulation)
+                    else if (a.PlanArchitecture != null)
                     {
                         minPerson.Add(a, Math.Min(3, a.EnoughPeople));
                         minTroop.Add(a, a.TroopReserveScale);
                     }
+                    else if (a.FrontLine || a.IsNetLosingPopulation)
+                    {
+                        minPerson.Add(a, Math.Min(3, a.EnoughPeople) / 2);
+                        minTroop.Add(a, a.TroopReserveScale);
+                    }
                     else
                     {
-                        minPerson.Add(a, 1);
+                        minPerson.Add(a, 0);
                         minTroop.Add(a, 0);
                     }
                     minFund.Add(a, Math.Min(a.FundCeiling, a.EnoughFund));
