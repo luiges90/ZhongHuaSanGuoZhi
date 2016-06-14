13a10d4489c944c5c68b02150c731d4d8e4c293c
Peter Wong <luiges90@gmail.com>
2016-06-14 10:41:22 +0800

    修正舊劇本事件不能觸發

--- GameObjects/GameObjects/GameScenario.cs
+++ GameObjects/GameObjects/GameScenario.cs
@@ -4703,8 +4703,6 @@
                             e.LoadArchitectureEffectFromString(this.GameCommonData.AllEventEffects, reader["ArchitectureEffect"].ToString());
                             e.LoadFactionEffectFromString(this.GameCommonData.AllEventEffects, reader["FactionEffect"].ToString());
 
-                            e.LoadYesEffectFromString(this.GameCommonData.AllEventEffects, reader["YesEffect"].ToString());
-                            e.LoadNoEffectFromString(this.GameCommonData.AllEventEffects, reader["NoEffect"].ToString());
                             try
                             {
                                 e.nextScenario = reader["NextScenario"].ToString();
@@ -4736,14 +4734,14 @@
                             {
 
                             }
-                           /* try
+                            try
                             {
                                 e.LoadYesEffectFromString(this.GameCommonData.AllEventEffects, reader["YesEffect"].ToString());
                                 e.LoadNoEffectFromString(this.GameCommonData.AllEventEffects, reader["NoEffect"].ToString());
                             }
                             catch
                             {
-                            }*/
+                            }
                             this.AllEvents.AddEventWithEvent(e);
                         }
                         catch (FormatException)
