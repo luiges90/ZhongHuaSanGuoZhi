0a869e222667a03141ccda331f34d0f170ccd5a4
Peter WONG <luiges90@gmail.com>
2016-10-26 22:25:00 +0800

    調整死亡機率

--- GameObjects/GameObjects/Person.cs
+++ GameObjects/GameObjects/Person.cs
@@ -1418,25 +1418,25 @@
                 yearDead += this.LongetivityIncreaseByInfluence;
 
                 if (yearDead - 5 <= base.Scenario.Date.Year && base.Scenario.Date.Year < this.YearDead &&
-                     GameObject.Random(60) == 0 && GameObject.Chance((6 - (yearDead - base.Scenario.Date.Year)) * 18))
+                     GameObject.Random(10) == 0 && GameObject.Chance((6 - (yearDead - base.Scenario.Date.Year)) * 18))
                 {
                     this.InjureRate -= 0.1f;
                     base.Scenario.GameScreen.OnOfficerSick(this);
                 }
                 else if (base.Scenario.Date.Year >= yearDead)
                 {
-                    if (this.DeadReason == PersonDeadReason.被杀死 && GameObject.Chance(10))
+                    if (this.DeadReason == PersonDeadReason.被杀死 && GameObject.Chance(50))
                     {
                         this.InjureRate -= (base.Scenario.Date.Year - yearDead + 1) * 0.1f;
                         base.Scenario.GameScreen.OnOfficerSick(this);
                     }
-                    else if (this.DeadReason == PersonDeadReason.郁郁而终 && GameObject.Chance(10) &&
+                    else if (this.DeadReason == PersonDeadReason.郁郁而终 && GameObject.Chance(50) &&
                         (this.BelongedFaction == null || this.Status == PersonStatus.Captive || this.BelongedFaction.ArchitectureTotalSize <= 8))
                     {
                         this.InjureRate -= (base.Scenario.Date.Year - yearDead + 1) * 0.1f;
                         base.Scenario.GameScreen.OnOfficerSick(this);
                     }
-                    else if (this.DeadReason == PersonDeadReason.操劳过度 && GameObject.Chance(10) &&
+                    else if (this.DeadReason == PersonDeadReason.操劳过度 && GameObject.Chance(50) &&
                         this.InternalExperience + this.StratagemExperience + this.TacticsExperience 
                         + this.BubingExperience + this.QibingExperience + this.NubingExperience + this.ShuijunExperience + this.QixieExperience >= 30000)
                     {
