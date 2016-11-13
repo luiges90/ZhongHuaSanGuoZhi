5dc8206ba4b23c248fdfc0160873632b95587450
yueluowujiang <375659963@qq.com>
2016-11-13 17:40:46 +0800

    增加大信息记录框

--- GameRecordPlugin/GameRecordPlugin/GameRecord.cs
+++ GameRecordPlugin/GameRecordPlugin/GameRecord.cs
@@ -28,6 +28,13 @@
         internal Texture2D ToolSelectedTexture;
         internal Texture2D ToolTexture;
 
+        private bool isRecord1Showing=false;
+        internal Rectangle Tool1Client;
+        internal Texture2D Tool1DisplayTexture;
+        internal Texture2D Tool1SelectedTexture;
+        internal Texture2D Tool1Texture;
+
+
         internal void AddBranch(GameObject gameObject, string branchName, Point position)
         {
             int num = this.Record.TopAddGameObjectTextBranch(gameObject, this.TextTree.GetBranch(branchName));
@@ -61,16 +68,29 @@
         {
             Rectangle? sourceRectangle = null;
             spriteBatch.Draw(this.ToolDisplayTexture, this.ToolDisplayPosition, sourceRectangle, this.ButtonDrawingColor, 0f, Vector2.Zero, SpriteEffects.None, 0.099f);
+            spriteBatch.Draw(this.Tool1DisplayTexture, this.Tool1DisplayPosition, sourceRectangle, this.ButtonDrawingColor, 0f, Vector2.Zero, SpriteEffects.None, 0.099f);
             if (this.IsRecordShowing)
             {
-                spriteBatch.Draw(this.RecordBackgroundTexture, this.RecordDisplayPosition, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.1f);
-                this.Record.Draw(spriteBatch, 0.0999f);
+                spriteBatch.Draw(this.RecordBackgroundTexture, this.RecordDisplayPosition, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.102f);
+                this.Record.Draw(spriteBatch, 0.101f);
             }
+
+            if (this.IsRecord1Showing)
+            {
+                 Record.DisplayOffset = new Point(500, 100);
+                spriteBatch.Draw(this.RecordBackgroundTexture, new Rectangle(500, 100, 500, 600), null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0.1f);
+                Record.Draw(spriteBatch, 0.0999f);
+            }
+
         }
 
         private Rectangle GetPositionRectangle(int i)
         {
             return new Rectangle(this.PositionRectangles[i].X + this.RecordDisplayOffset.X, this.PositionRectangles[i].Y + this.RecordDisplayOffset.Y, this.PositionRectangles[i].Width, this.PositionRectangles[i].Height);
+        }
+        private Rectangle GetPositionRectangle1(int i)
+        {
+            return new Rectangle(this.PositionRectangles[i].X + 500, this.PositionRectangles[i].Y + 100, this.PositionRectangles[i].Width, this.PositionRectangles[i].Height);
         }
 
         internal void Initialize(Screen screen)
@@ -103,18 +123,40 @@
                 if (StaticMethods.PointInRectangle(position, this.ToolDisplayPosition))
                 {
                     this.IsRecordShowing = !this.IsRecordShowing;
+                    if (this.IsRecordShowing)
+                        this.IsRecord1Showing = false;
                 }
-                if (this.IsRecordShowing)
+
+                if (StaticMethods.PointInRectangle(position, this.Tool1DisplayPosition))
+                {
+                    this.IsRecord1Showing = !this.IsRecord1Showing;
+                    if (this.IsRecord1Showing)
+                        this.IsRecordShowing = false;
+                }
+
+                if (this.IsRecordShowing )
                 {
                     for (int i = 0; i < this.PositionRectangles.Count; i++)
                     {
-                        if (StaticMethods.PointInRectangle(position, this.GetPositionRectangle(i)))
+                        if (StaticMethods.PointInRectangle(position, this.GetPositionRectangle(i)) && position.Y<screen.viewportSize.Y-48)
                         {
                             this.screen.JumpTo(this.Positions[i]);
                             break;
                         }
                     }
                 }
+                if (this.IsRecord1Showing)
+                {
+                    for (int i = 0; i < this.PositionRectangles.Count; i++)
+                    {
+                        if (StaticMethods.PointInRectangle(position, this.GetPositionRectangle1(i)) && position.Y < screen.viewportSize.Y - 48)
+                        {
+                            this.screen.JumpTo(this.Positions[i]);
+                            break;
+                        }
+                    }
+                }
+
             }
         }
 
@@ -244,6 +286,26 @@
             get
             {
                 return new Rectangle(this.ToolClient.X + this.DisplayOffset.X, this.ToolClient.Y + this.DisplayOffset.Y, this.ToolClient.Width, this.ToolClient.Height);
+            }
+        }
+
+        internal bool IsRecord1Showing
+        {
+            get
+            {
+                return this.isRecord1Showing;
+            }
+            set
+            {
+                this.isRecord1Showing = value;
+            }
+        }
+
+        private Rectangle Tool1DisplayPosition
+        {
+            get
+            {
+                return new Rectangle(this.Tool1Client.X + this.DisplayOffset.X, this.Tool1Client.Y + this.DisplayOffset.Y, this.Tool1Client.Width, this.Tool1Client.Height);
             }
         }
     }
