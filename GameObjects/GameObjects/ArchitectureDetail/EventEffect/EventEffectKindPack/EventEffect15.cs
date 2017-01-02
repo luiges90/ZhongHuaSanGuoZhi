7398c568b6a7251211a95fa4468b0352d679f066
Peter WONG <luiges90@gmail.com>
2017-01-02 11:06:34 +0800

    新增能力經驗效果

---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect15.cs
@@ -0,0 +1,27 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+    internal class EventEffect15 : EventEffectKind
+    {
+        private int increment;
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            person.CommandExperience += increment;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.increment = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+
---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect25.cs
@@ -0,0 +1,27 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+    internal class EventEffect25 : EventEffectKind
+    {
+        private int increment;
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            person.IntelligenceExperience += increment;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.increment = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+
