9d9bb62a1a09ffa8460c9e672b6921992d9dbe23
Peter WONG <luiges90@gmail.com>
2017-05-20 22:28:30 +0800

    影響類286就是285，加上可選條件

---
+++ GameObjects/GameObjects/Influences/InfluenceKindPack/InfluenceKind286.cs
@@ -0,0 +1,42 @@
+﻿namespace GameObjects.Influences.InfluenceKindPack
+{
+    using GameObjects;
+    using GameObjects.Influences;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class InfluenceKind286 : InfluenceKind
+    {
+        private int conditionID;
+
+       
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.conditionID = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+
+        public override bool IsVaild(Person person)
+        {
+            Condition t = person.Scenario.GameCommonData.AllConditions.GetCondition(conditionID);
+            if (t.CheckCondition(person))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+        
+        
+         
+
+
+
+
+    
